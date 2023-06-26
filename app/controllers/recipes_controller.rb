d16e817129cdf84fa2f5146151be50afc3749363
Progress2002 <ezeamakachisom2002@gmail.com>
2023-06-26 22:05:40 +0100

    Add recipe controller

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,60 @@
+class RecipesController < ApplicationController
+  # before_action :set_recipe, only: %i[ show edit destroy ]
+
+  # GET /recipes or /recipes.json
+  def index
+    @recipes = Recipe.all
+  end
+
+  # GET /recipes/1 or /recipes/1.json
+  def show
+  end
+
+  # GET /recipes/new
+  def new
+    @recipe = Recipe.new
+  end
+
+  # GET /recipes/1/edit
+  def edit
+  end
+
+  # POST /recipes or /recipes.json
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+  
+    if @recipe.save
+      redirect_to recipe_url(@recipe), notice: "Recipe was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /recipes/1 or /recipes/1.json
+  # def update
+  #   if @recipe.update(recipe_params)
+  #     redirect_to recipe_url(@recipe), notice: "Recipe was successfully updated." 
+  #   else
+  #     render :edit, status: :unprocessable_entity
+  #   end
+  # end
+
+  # DELETE /recipes/1 or /recipes/1.json
+  def destroy
+    @recipe.destroy
+
+    redirect_to recipes_url, notice: "Recipe was successfully destroyed." 
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def recipe_params
+      params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+    end
+end
