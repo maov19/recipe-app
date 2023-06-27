ef08b2f27ecfe0603c9770598d6883dc7a7caf3a
Progress2002 <ezeamakachisom2002@gmail.com>
2023-06-27 22:44:20 +0100

    Add name filed to login form, and add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
-  has_many :foods
+  has_many :foods, dependent: :destroy
   # has_many :recipes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :recipes
+  has_many :recipes, dependent: :destroy
+  validates :name, presence: true
 end
