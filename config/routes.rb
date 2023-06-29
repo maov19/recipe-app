94d0de78baee55096e6bd899759959fb7ca49a5a
Progress2002 <ezeamakachisom2002@gmail.com>
2023-06-29 10:49:39 +0100

    Add route path and view files for recipe_food

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :recipes, except: [:update] do
+    resources :recipe_foods, except: [:index]
     patch 'toggle_public', on: :member
+    delete 'recipe_foods/:id', to: 'recipe_foods#destroy', as: 'delete_recipe_food'
   end
   get "/public_recipes", to: "public_recipes#index"
-  
 
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
