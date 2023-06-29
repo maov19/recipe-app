7bbec911634861c638ae0b3f406891c70c7c35ec
Progress2002 <ezeamakachisom2002@gmail.com>
2023-06-29 10:51:26 +0100

    Update recipe controller and add validations to recipe model

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,23 +1,24 @@
 class RecipesController < ApplicationController
-  # GET /recipes or /recipes.json
   def index
     @recipes = Recipe.where(user_id: current_user.id)
   end
 
-  # GET /recipes/1 or /recipes/1.json
   def show
     @recipe = Recipe.find(params[:id])
+    @recipe_foods = @recipe.recipe_foods.includes(:food)
   end
 
-  # GET /recipes/new
+  def toggle_public
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(public: !@recipe.public)
+
+    redirect_to recipe_path(@recipe)
+  end
+
   def new
     @recipe = Recipe.new
   end
 
-  # GET /recipes/1/edit
-  def edit; end
-
-  # POST /recipes or /recipes.json
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user = current_user
@@ -29,16 +30,6 @@ class RecipesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /recipes/1 or /recipes/1.json
-  # def update
-  #   if @recipe.update(recipe_params)
-  #     redirect_to recipe_url(@recipe), notice: "Recipe was successfully updated."
-  #   else
-  #     render :edit, status: :unprocessable_entity
-  #   end
-  # end
-
-  # DELETE /recipes/1 or /recipes/1.json
   def destroy
     @recipe = current_user.recipes.find(params[:id])
     @recipe.destroy
@@ -48,18 +39,8 @@ class RecipesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  # def set_recipe
-  #   @recipe = Recipe.find(params[:id])
-  # end
-
   # Only allow a list of trusted parameters through.
   def recipe_params
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
-  # def recipe_params
-  #   params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description).tap do |whitelisted|
-  #     whitelisted[:public] = false unless params[:recipe][:public].present?
-  #   end
-  # end
 end
