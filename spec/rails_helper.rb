89519dffc51c84a48a48ef71eea21603b4bb4b7b
mossa <martin.ossa.8@gmail.com>
2023-06-30 12:24:50 +0200

    Update rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-# spec/rails_helper.rb
-# require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-# Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
-
-RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
-  config.infer_spec_type_from_file_location!
-  config.filter_rails_from_backtrace!
-=======
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -80,5 +60,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
->>>>>>> 9253e7edbea24e957124aadb85d2d08270a6336d
 end
